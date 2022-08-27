aee958ce0fcf8a504d411cf84a77489b30e58aa1
alemartinn <ale_xeg147@hotmail.com>
2022-08-27 14:34:16 -0300

    firsts req and res

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+// req = request
+// res = response
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  /*headers, body*/
+  // res.status(404).json()
+  // res.render('index', { title: 'Express' });
+  /*Va a generar un json porque trabajamos con json*/
+  // res.json({
+  //   "paging":{
+  //     "total":1,
+  //     "almost":3
+  //   }
+  // }); 
+  res.json([])
+});
+
+router.get('/:id', function(req, res, next){
+
+  if (req.params.id == 123){
+    res.status(404).json()
+    return
+  }
+
+  res.json({
+    id:req.params.id
+  })
+})
+
+module.exports = router;
