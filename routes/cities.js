366e736c50081fe6ef00f776b0696300cd4c0ab7
alemartinn <ale_xeg147@hotmail.com>
2022-08-31 01:08:09 -0300

    preparing to update and remove

--- routes/cities.js
+++ routes/cities.js
@@ -2,13 +2,16 @@ var express = require('express'); //metodo express
 var router = express.Router();  //Trae un enrutador de express. Objeto.
 
 // const eventController = require('../controllers/cityController');
-const {create, read} = require('../controllers/cityController');
+const {create, read, readAll, update, destroy} = require('../controllers/cityController');
 
 /* GET users listing. */
 
 /*Le agregamos un metodo para que controle el controlador (de cities en este caso)*/
 // router.post('/events', eventController.create);
 router.post('/', create);
+router.get('/',readAll);
 router.get('/:id', read);
+// router.get('/',update);
+// router.get('/',remove);
 
 module.exports = router;
