19ead9f1ef87644cf9c97dfb8d1abd3b6dc89218
alemartinn <ale_xeg147@hotmail.com>
2022-08-31 01:02:28 -0300

    file to generates some cities

---
+++ buildingCities.js
@@ -0,0 +1,14 @@
+
+require('dotenv').config();
+
+const db = require('./config/database');
+
+const Cities = require('./models/City');
+
+Cities.create({
+    city: "London",
+    country: "United Kingdom",
+    photo: "https://w0.peakpx.com/wallpaper/977/956/HD-wallpaper-palace-of-westminster-london-houses-of-parliament-river-thames-westminster-bridge-westminster-abbey-evening-sunset-england.jpg",
+    population: "30000",
+    fundation: "08-21-2022"
+});
