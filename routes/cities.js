7c9601c931c8e40c916ba8e1873abc290f094bf3
alemartinn <ale_xeg147@hotmail.com>
2022-08-31 23:45:26 -0300

    defining names of methods

--- routes/cities.js
+++ routes/cities.js
@@ -1,17 +1,17 @@
 var express = require('express'); //metodo express
 var router = express.Router();  //Trae un enrutador de express. Objeto.
 
-// const eventController = require('../controllers/cityController');
-const {create, read, readAll, update, destroy} = require('../controllers/cityController');
+// const cityController = require('../controllers/cityController');
+const {createCity, readAll, readCity, updateCity, deleteCity} = require('../controllers/cityController');
 
 /* GET users listing. */
 
 /*Le agregamos un metodo para que controle el controlador (de cities en este caso)*/
-// router.post('/events', eventController.create);
-router.post('/', create);
-router.get('/',readAll);
-router.get('/:id', read);
-// router.get('/',update);
-// router.get('/',remove);
+// router.post('/cities', cityController.create);
+router.post('/', createCity);
+router.get('/', readAll);
+router.get('/:id', readCity);
+router.put('/:id', updateCity);
+router.delete('/:id', deleteCity);
 
 module.exports = router;
