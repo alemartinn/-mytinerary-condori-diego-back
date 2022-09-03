5ae9682acb20b7c2ff0f6bb68f0a3f8eeb456d02
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-02 21:21:39 -0300

    Add router user

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const userRouter = require('./users')
 const cityRouter = require('./cities')
+const prueba = require('./prueba')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,5 +15,6 @@ router.get('/', function(req, res, next) {
 /* Va a unir todas las rutas en index */
 router.use('/cities', cityRouter)
 router.use('/users', userRouter)
+router.use('/prueba', prueba)
 
 module.exports = router;
