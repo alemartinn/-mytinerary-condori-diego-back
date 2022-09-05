5b30ae9e69b681588d56472c710d172387f0a752
alemartinn <ale_xeg147@hotmail.com>
2022-09-05 00:07:02 -0300

    using a new method with router

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const {createItinerary} = require('../controllers/itineraryController');
+const {createItinerary, updateItinerary} = require('../controllers/itineraryController');
 
 router.post('/', createItinerary);
+router.patch('/:id', updateItinerary);
+
 module.exports = router;
