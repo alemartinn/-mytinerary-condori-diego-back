8274dc1047c28a9a2bd965b5b530085bf3665719
alemartinn <ale_xeg147@hotmail.com>
2022-09-04 23:53:25 -0300

    remove innecesaries lines

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -1,13 +1,3 @@
-let schema = new mongoose.Schema({
-    name: {type: String, required: true},
-    user: {type: String,},
-    city: {type: String},
-    price: {type: Number, required: true},
-    likes: {type: Array, required: true},
-    tags: {type: Array, required: true},
-    duration: {type: Number, required: true}
-});
-
 const Itinerary = require('../models/Itinerary');
 
 const itineraryController ={
