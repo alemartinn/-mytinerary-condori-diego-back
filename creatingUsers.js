48bdc8c9ad824f3a9857f3670d94159c65fa2897
alemartinn <ale_xeg147@hotmail.com>
2022-09-05 00:49:01 -0300

    we added a script to create some users

--- creatingUsers.js
+++ creatingUsers.js
@@ -1,15 +1,16 @@
 
 require('dotenv').config();
+const db = require('./config/database')
 
 const User = require('./models/User');
 
-// for (let i = 0; i < 3; i++){
+for (let i = 0; i < 3; i++){
 User.create({
-    name: `Alejandro2`,
+    name: `MITYNER-${i+1}`,
     lastName: "martinnnn",
     mail: "alemartin@gmail.com",
     password: "1234",
     photo: "foto.jpg",
     country: "Argentina"
 })
-// }
+}
