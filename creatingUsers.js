6805c80d55c1d9c05e90eecd6216a17bf07c6253
alemartinn <ale_xeg147@hotmail.com>
2022-09-07 15:31:58 -0300

    fixing scripts to generate default data

--- creatingUsers.js
+++ creatingUsers.js
@@ -4,13 +4,12 @@ const db = require('./config/database')
 
 const User = require('./models/User');
 
-for (let i = 0; i < 3; i++){
-User.create({
-    name: `MITYNER-${i+1}`,
-    lastName: "martinnnn",
-    mail: "alemartin@gmail.com",
-    password: "1234",
-    photo: "foto.jpg",
-    country: "Argentina"
+const ourUsers = [
+    {name:"Ale", lastName:"Jandro",mail:"alejandro14@gmail.com",password:"1234",photo:"foto.jpg", country:"Argentina"},
+    {name:"Martin", lastName:"nnnnnn", mail:"martin14@gmail.com", password:"4321", photo:"asasd.jpg", country:"Argentina"},
+    {name:"Diegotex", lastName:"Acuña", mail:"nahuel@gmail.com", password:"1111", photo:"img.jpg", country:"Argentina"}
+]
+
+ourUsers.forEach((element)=>{
+    User.create(element)
 })
-}
