e49c62e419014812a5304785e50651f88f460840
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-07 20:21:36 -0300

    Update with populate

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let schema = new mongoose.Schema({
     comment: {type: String, required: true},
     user: {type: String},
-    itinerary: {type: String}
+    itinerary: {type: mongoose.Types.ObjectId, ref: 'itineraries'}
 })
 
 const Comment = mongoose.model(
