01989490cb71075a2026052859ffbd1ff7f34001
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-08 00:14:46 -0300

    New script to create commentaries

---
+++ creatingComments.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const db = require('./config/database');
+
+const Comment = require('./models/Comment');
+
+const ourComments = [
+    {
+        comment: "It's Awesome",
+        user: "6312a119918824959c257db2",
+        itinerary: "63191e5019e820ede7918c5b"
+    },
+    {
+        comment: "GREAT!!",
+        user: "6312a13de68737e0c527a1bb",
+        itinerary: "63191e5019e820ede7918c5e"
+    }
+]
+
+ourComments.forEach(commentary => Comment.create(commentary));
