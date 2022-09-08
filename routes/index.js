acdc6c66c6cc9e92ea404d4db186c24d6e914f6f
alemartinn <ale_xeg147@hotmail.com>
2022-09-07 23:50:51 -0300

    implemented activities on index

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const userDefaultRouter = require('./usersDefault');
 const cityRouter = require('./cities');
 const user = require('./users');
 const itineraryRouter = require('./itineraries');
+const activitiesRouter = require('./activities');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,5 +19,6 @@ router.use('/cities', cityRouter);
 router.use('/usersDefault', userDefaultRouter);
 router.use('/users', user);
 router.use('/itineraries', itineraryRouter);
+router.use('/activities', activitiesRouter);
 
 module.exports = router;
