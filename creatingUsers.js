6c45de138f927e4cdceb9f8a4c4041fe4b58cb74
alemartinn <ale_xeg147@hotmail.com>
2022-09-08 07:08:15 -0300

    fixing some ids

--- creatingUsers.js
+++ creatingUsers.js
@@ -5,9 +5,9 @@ const db = require('./config/database');
 const User = require('./models/User');
 
 const ourUsers = [
-    {name:"Ale", lastName:"Jandro",mail:"alejandro14@gmail.com",password:"1234",photo:"foto.jpg", country:"Argentina"},
-    {name:"Martin", lastName:"nnnnnn", mail:"martin14@gmail.com", password:"4321", photo:"asasd.jpg", country:"Argentina"},
-    {name:"Diegotex", lastName:"Acuña", mail:"nahuel@gmail.com", password:"1111", photo:"img.jpg", country:"Argentina"}
+    {name:"Ale", lastName:"Jandro",mail:"alejandro14@gmail.com",password:"1234",photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", country:"Argentina"},
+    {name:"Martin", lastName:"nnnnnn", mail:"martin14@gmail.com", password:"4321", photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", country:"Argentina"},
+    {name:"Diegotex", lastName:"Acuña", mail:"nahuel@gmail.com", password:"1111", photo:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", country:"Argentina"}
 ];
 
 ourUsers.forEach(element=>User.create(element));
