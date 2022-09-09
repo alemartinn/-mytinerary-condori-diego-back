633ecf93f7e9287958dbf229d036d5b7e56943ab
alemartinn <ale_xeg147@hotmail.com>
2022-09-09 16:18:43 -0300

    new method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,75 @@
 const User = require('../models/User');
+const crypto = required('crypto');
+const bcryptjs = required('bcryptjs');
 
 const userController ={
+
+    signUp: async(req, res) => {
+        const {name, photo, email, password, role, from} = req.body;
+        try{
+            let user = await User.findOne({email})
+            if (!user){
+                let logged = false;
+                let verified = false;
+                //code: clave unica de usuario o unique string.
+                let code = crypto.randomBytes(15).toString('hex'); 
+                
+                if (from === 'form'){
+                    // Hash: convertir una password en una password que nadie pueda saber que esta referenciada.
+                    // No guardar passwords sin hacer hash.
+                    password = bcryptjs.hashSync(password, 10); // 10: nvl seguridad.
+                    user = await new User({name, photo, email, password, role, from, logged, verified, code}).save();
+                    //Incorporar funcion para envio de mail de verificacion.
+                    res.status(201).json({
+                        message: "User signed up.",
+                        success: true
+                    });
+                }else{
+                    password = bcryptjs.hashSync(password, 10); // 10: nvl seguridad.
+                    verified = false;
+                    user = await new User({name, photo, email, password, role, from: [from], logged, verified, code}).save();
+                    //Incorporar funcion para envio de mail de verificacion.
+                    res.status(201).json({
+                        message: "User signed up.",
+                        success: true
+                    });
+                }
+                user = await new User({name, photo, email, password, role, logged, verified, code}).save();
+            } else {
+                if (user.from.includes(from)){
+                    res.status(200).json({
+                        message: "User already registered",
+                        success: false // Porque no completo el registro.
+                    });
+                } else{
+                    user.from.push(from); //Agrego nuevo origen de registro.
+                    user.verified = true;
+                    user.password.push(bcryptjs.hashSync(password,10));
+                    await user.save();
+                    res.status(201).json({
+                        message: "User signed up with " + from,
+                        success: true
+                    })
+                }
+            }
+        }
+        catch(error){
+            console.log(error);
+            res.status(400).json({
+                message: "Couldn't signed up",
+                success: false
+            })
+        }
+    },
+    verifyMail: async(req, res) => {
+
+    },
+    signIn: async(req, res) => {
+
+    },
+    signOut: async(req, res) => {
+
+    },
     createUser: async(req, res) => {
         const {name,lastName, mail, password, photo, country} = req.body;
         try{
