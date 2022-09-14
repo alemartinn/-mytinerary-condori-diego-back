bd66a73ddb0be160420e9eb318cbcf0e456bbb5d
alemartinn <ale_xeg147@hotmail.com>
2022-09-14 17:02:15 -0300

    fixing files

--- creatingActivities.js
+++ creatingActivities.js
@@ -4,10 +4,22 @@ const db = require('./config/database');
 
 const Activity = require('./models/Activity');
 
-const ourActivities = [
-    {name: 'Activity One', photo: 'activity_one.jpg', itinerary: '6319d9d9d227c639cd7b6731'},
-    {name: 'Activity Two', photo: 'activity_two.jpg', itinerary: '6319d9d9d227c639cd7b6731'},
-    {name: 'Activity Two Duplicate', photo: 'activity_two_duplicate.jpg', itinerary: '6319d9d9d227c639cd7b6732'}
+const idItineraries = [
+    '6319d9d9d227c639cd7b6731',
+    '6319d9d9d227c639cd7b6732',
+    '6319d9d9d227c639cd7b6749'
 ]
 
-ourActivities.forEach(element => Activity.create(element));
+
+// ourActivities.forEach(element => Activity.create(element));
+
+idItineraries.forEach(idItinerary => {
+    const ourActivities = [
+    {name: 'Activity One', photo: 'activity_one.jpg', itinerary: idItinerary},
+    {name: 'Activity Two', photo: 'activity_two.jpg', itinerary: idItinerary},
+    {name: 'Activity Two Duplicate', photo: 'activity_two_duplicate.jpg', itinerary: idItinerary}
+    ]
+    Activity.create(ourActivities[0]);
+    Activity.create(ourActivities[1]);
+    Activity.create(ourActivities[2]);
+});
