d6b2e702522b397fec325ad3e745094373920e11
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-14 01:10:50 -0300

    change logged to loggedIn

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ let schema = new mongoose.Schema({
     photo: {type: String, required: true},
     country: {type: String, required: true},
     from: [{type: String, required: true}],
-    logged: {type:Boolean, required: true},
+    loggedIn: {type:Boolean, required: true},
     verified: {type:Boolean, required: true},
     code: {type:String, required:true}
 });
