47ed440ee46a90d779b7ad265ac48f2908280e8d
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-14 04:36:53 -0300

    create valid model

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 let schema = new mongoose.Schema({
-    name: {type: String, required: true},
+    name: {type: String, required: true, min: 2, max:12},
     user: {
         type: mongoose.Types.ObjectId,
         ref: 'users'
@@ -10,10 +10,10 @@ let schema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         ref: 'cities'
     },
-    price: {type: Number, required: true},
+    price: {type: Number, required: true, min:0},
     likes: {type: Array, required: true},
     tags: {type: Array, required: true},
-    duration: {type: Number, required: true}
+    duration: {type: Number, required: true, min:0, max:24}
 });
 
 const Itinerary = mongoose.model(
