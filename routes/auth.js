61a2263303783e777d54c0c95d0407e6c7311acf
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-13 23:07:18 -0300

    add signOut route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {createUser, readUser, updateUser, deleteUser, signUp} = require('../controllers/userController');
+const {createUser, readUser, updateUser, deleteUser, signUp, signOut} = require('../controllers/userController');
 
 router.post('/', signUp);
 // router.post('/', createUser);
+router.post('/:id', signOut)
 router.get('/:id', readUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
