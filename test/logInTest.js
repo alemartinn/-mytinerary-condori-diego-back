0210d44ce8a8611b6a801de6dd461679475bfa17
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-16 05:28:05 -0300

    added tests to log in method

---
+++ test/logInTest.js
@@ -0,0 +1,43 @@
+const request = require('supertest')
+const app = require('../app')
+const { assert } = require('chai')
+
+ describe('POST /auth/signin', function () {
+    it('Must respond with the name', function(){
+        request(app)
+            .post('/auth/signin')
+            .send({
+            mail:"alexeg147@gmail.com",
+            password: "alejandro",
+            from: 'form'
+            })
+            .then(response => {
+                user = response.body.name
+                assert.isString(response.body.name)
+                done()
+            })
+    })
+
+    it('Must respond with 400 status code', function(done){
+        request(app)
+            .post('/signin')
+            .send({
+                mail:"alexeg147@gmail.com",
+                password: "nosoyale",
+                from: 'form'
+            })
+            .expect(400)
+            done()
+    })
+
+    it('Must respond with 404 status code', function(done){
+        request(app)
+            .post('/signin')
+            .send({})
+            .expect(404, done)
+
+
+    })
+
+
+     })
