1d06ff419691ebef2ec31035118e2df46d470e57
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-16 07:16:10 -0300

    add router update and delete comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { createComment, getAllComments, getComment} = require('../controllers/commentController');
+const { createComment, getAllComments, getComment, updateComment, deleteComment} = require('../controllers/commentController');
 
 router.post('/', createComment);
 router.get('/', getAllComments);
 router.get('/:id', getComment);
+router.patch('/:id', updateComment);
+router.patch('/:id', deleteComment)
 
 module.exports = router
