9eba7fa8aa4fd7b00066f20f81853a742eaacd5f
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-21 16:55:08 -0300

    add likedislike route

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const passport = require('../config/passport');
 
-const {createItinerary, getAllItineraries, updateItinerary, deleteItinerary} = require('../controllers/itineraryController');
+const {createItinerary, getAllItineraries, updateItinerary, deleteItinerary,likeDislike} = require('../controllers/itineraryController');
 
 router.get('/', getAllItineraries)
 router.post('/', createItinerary);
