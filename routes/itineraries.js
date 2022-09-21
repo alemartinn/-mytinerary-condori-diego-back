80acc4ae19f47db10d9f9ff8259ea0eb030aeb3c
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-21 16:49:06 -0300

    new route to method get an itinerary

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const {createItinerary, getAllItineraries, updateItinerary, deleteItinerary} = require('../controllers/itineraryController');
+const {createItinerary, getOneItinerary, getAllItineraries, updateItinerary, deleteItinerary} = require('../controllers/itineraryController');
 
 router.get('/', getAllItineraries)
+router.get('/:id', getOneItinerary)
 router.post('/', createItinerary);
 router.patch('/:id', updateItinerary);
 router.delete('/:id', deleteItinerary);
