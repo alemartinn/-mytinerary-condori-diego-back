1e003c23067124ef8cf9e8d2780ff41ccd483c60
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-23 20:44:28 -0300

    add favorites comment

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { signUp, verifyMail, signIn, signOut, updateUser } = require('../controllers/userController');
+const { signUp, verifyMail, signIn, signOut, updateUser, myFavorites } = require('../controllers/userController');
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/signout', signOut);
 router.get('/verify/:code', verifyMail);
 router.patch('/updateuser/:id', updateUser);
+router.patch('/favorites/:id', myFavorites)
 
 module.exports = router;
